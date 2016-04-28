e0a50976ecde3b1c44fb3451df2c647569af93d0
songerwei <songerwei@xxtao.com>
2016-04-28 21:58:20 +0800

    require

---
+++ require/js/index.js
@@ -0,0 +1,8 @@
+// AUTHOR:   SongErwei
+// ROLE:    	
+// CREATED:  2016-04-28 21:39:55
+ 
+requirejs(['utils'], function(utils) {
+    utils.song;
+    utils.foo();
+})
---
+++ require/js/utils.js
@@ -0,0 +1,12 @@
+// AUTHOR:   SongErwei
+// ROLE:    	
+// CREATED:  2016-04-28 21:42:31
+ 
+define({
+    song: "song",
+    hello: "hello",
+
+    foo: function() {
+        alert('foo');
+    }
+});

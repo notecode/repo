e5648cf504523e9e3f749412b7d57e257c48f152
songerwei <songerwei@xxtao.com>
2016-08-20 01:15:59 +0800

    pass: <script> jquery require

--- amap-require/js/require-init.js
+++ amap-require/js/require-init.js
@@ -1,12 +1,23 @@
 require.config({　　　　
 	paths: {　　　　　　
-		"amap": "http://webapi.amap.com/maps?v=1.3&key=&callback=init"　　　　
+	//	"amap": "http://webapi.amap.com/maps?v=1.3&key=&callback=init"　　　　
+        "amap": "http://webapi.amap.com/maps?v=1.3&key=24a59ee2492ddf5fba6e8208f6383560&plugin=AMap.CitySearch&callback=init"
 	}　　
 });
-require(['amap'], function() {
-	window.init = function() {
-		require(['require-initMap'], function(mapIniter) {
-			mapIniter.init();
-		})
-	}
+
+// require(['amap'], function() {
+// 	window.init = function() {
+// 		require(['require-initMap'], function(mapIniter) {
+// 			mapIniter.init();
+// 		})
+
+//         var map = new AMap.Map('container');
+// 	}
+// })
+//
+
+$(function() {
+    require(['amap'], function() {
+        var map = new AMap.Map('container');
+    })
 })

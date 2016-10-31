000b5f9c674e24ae302ba5330c2500651f5ae12d
songerwei <songerwei@xxtao.com>
2016-10-31 19:47:15 +0800

    整理目录

--- backend/php/Angel/src/public/index.php
+++ backend/php/Angel/src/public/index.php
@@ -3,7 +3,7 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-require '../../../vendor/autoload.php';
+require '../../vendor/autoload.php';
 require 'db.php';
 
 $app = new \Slim\App;

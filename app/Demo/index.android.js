8a598d6ceb005eb26834974c4685f3f5e6d61ef2
songerwei <songerwei@xxtao.com>
2016-11-18 16:09:49 +0800

    android先照搬一下ios的代码，还不会代码共用

--- app/Demo/index.android.js
+++ app/Demo/index.android.js
@@ -5,48 +5,104 @@
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, ListView, Text, View, StyleSheet } from 'react-native';
+import axios from 'axios';
+import extend from 'extend';
 
-export default class Demo extends Component {
+class Demo extends Component {
+  constructor(props) {
+    super(props);
+    this.api = 'http://api.xxtao.com/index.php'
+    //this.fetch_j();
+    this.axios_j();
+    this.axios_demand_list();
+    this.state = {dataSource: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})};
+  }
+
+  axios_demand_list() {
+    var _this = this;
+    this.axios_get('demand/b37', {city_id: '151'}, {
+      succ: function(json) {
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        _this.setState({
+          dataSource: ds.cloneWithRows(json.list)
+        });
+      },
+      fail: function(json) {
+      }
+    });
+  }
+
+  axios_j() {
+    this.axios_get('j', {});
+  }
+
+  axios_get(r, q, cb) {
+    function tlog(text) {
+      console.log(text);
+    }
+    function olog(pre, obj) {
+      var t = pre + JSON.stringify(obj);
+      var max = 500;
+      if (t.length < max) {
+        tlog(t)
+      } else {
+        tlog(t.substring(0, max) + ' ... [total: ' + t.length + ']')
+      }
+    }
+
+    axios.get(this.api, {
+      params: extend({
+        'r': r
+      }, q) 
+    })
+    .then(function (response) { 
+      var resp = response.data;
+      olog('axios resp:', resp);
+
+      cb && cb.always && cb.always(resp);
+      if (1 == resp.succ) {
+        cb && cb.succ && cb.succ(resp);
+      } else {
+        cb && cb.fail && cb.fail(resp);
+      }
+    })
+    .catch(function (error) {
+      tlog(error);
+      cb && cb.always && cb.always(resp);
+      cb && cb.fail && cb.fail({});
+    });
+  }
+
+  fetch_j() {
+    fetch(this.api + '?r=j')
+    .then((response) => response.text())
+    .then((responseText) => {
+      console.log('fetch resp: ' + responseText);
+    })
+    .catch((error) => {
+      console.warn(error);
+    });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+      <View style={{flex: 1, paddingTop: 22}}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text style={styles.row}>{rowData.nick} {rowData.size}</Text>}
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
+var styles = StyleSheet.create({
+  row: {
     fontSize: 20,
-    textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: '#656565',
+    color: '#fff'
   },
 });
 

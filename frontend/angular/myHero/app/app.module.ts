f115eb5da24e7f14e23a2b6b2c661680e1d012a6
songerwei <songerwei@xxtao.com>
2016-12-10 13:15:55 +0800

    hero detail, separate

--- frontend/angular/myHero/app/app.module.ts
+++ frontend/angular/myHero/app/app.module.ts
@@ -3,10 +3,11 @@ import { FormsModule }   from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, HeroDetailComponent ],
   bootstrap:    [ AppComponent ]
 })
 

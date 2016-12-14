71dc8824b00752b7b92304083a03d2d297ac9980
songerwei <songerwei@xxtao.com>
2016-12-14 12:37:13 +0800

    oLed: 首页banner（用老的html+scss）

---
+++ frontend/angular/oLed/app/components/head-styles/the.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'head-styles',
+  template: `
+  	<meta name="song"/>
+  	<link rel="stylesheet" href="./app/g-sass/libs/bootstrap.css">
+  `
+})
+export class HeadStylesComponent  {}
--- frontend/angular/oLed/app/modules/app.mod.ts
+++ frontend/angular/oLed/app/modules/app.mod.ts
@@ -2,11 +2,12 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }     from '../components/app/the';
+import { HeadStylesComponent }  from '../components/head-styles/the';
 import { BannerComponent }  from '../components/banner/the';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent, BannerComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HeadStylesComponent, BannerComponent ],
+  bootstrap:    [ AppComponent, HeadStylesComponent ]
 })
 export class AppModule { }

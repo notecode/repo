a8e6cd4dfa0a9cc36a1929f7629595607379f9d1
songerwei <songerwei@xxtao.com>
2017-02-14 16:59:15 +0800

    controller: hello

---
+++ backend/php/laravel/demo/app/Http/Controllers/Hello.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Hello extends Controller
+{
+    //
+    public function index() 
+    {
+        return 'I am hello controller';
+    }
+}

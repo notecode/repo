2215599563c25f204178c7237d242d0d7d1d2d7a
songerwei <songerwei@xxtao.com>
2017-02-27 20:37:10 +0800

    can scrape ganji.com's data now

--- backend/nodejs/scraping/index.js
+++ backend/nodejs/scraping/index.js
@@ -15,24 +15,63 @@ function rLog(t) {
   log(t.red);
 }
 
-//var url = 'https://www.upwork.com/o/jobs/browse/c/web-mobile-software-dev/';
-var url = 'http://localhost/upwork.com/';
+var url = 'http://www.ganji.com/index.htm';
+//var url = 'http://qd.ganji.com/fang5/h1/';
+index_page(url);
 
-scraperjs.StaticScraper.create(url)
-.onStatusCode(function(statusCode, utils) {
-  gLog('The target: ' + utils.url);
-  log(statusCode);
-  if (statusCode != 200) {
-    rLog('Forbidden');
-    utils.stop();
-  }
-}).scrape(function($) {
-  log('do scrape');
+function index_page(url) {
+  scraperjs.StaticScraper.create(url)
+  .onStatusCode(function(statusCode, utils) {
+    gLog('City-list page url: ' + utils.url);
+    log(statusCode);
+    if (statusCode != 200) {
+      rLog('Forbidden');
+      utils.stop();
+    }
+  }).scrape(function($) {
+    var city = $('.all-city dl dd a');
+    return city.map(function () {
+      return {name: $(this).text(), home: $(this).attr('href')};
+    }).get();
+  })
+  .then(function(href_list) {
+    var url = href_list[0].home + '/fang5/';
+    house_list_page(url);
 
-  return $("a[href*='job/_~']").map(function() {
-    return $(this).attr('href');
-  }).get();
-})
-.then(function(news) {
-  log(news);
-})
+//    href_list.forEach(function(href) {
+//      var url = href.home + '/fang5/';
+//      log(url);
+//      //house_list_page(url);
+//    });
+  });
+}
+
+function house_list_page(url) {
+  scraperjs.StaticScraper.create(url)
+  .onStatusCode(function(statusCode, utils) {
+    gLog('House-list page url: ' + utils.url);
+    log(statusCode);
+    if (statusCode != 200) {
+      rLog('Forbidden');
+      utils.stop();
+    }
+  }).scrape(function($) {
+    var price = $('.time');
+    return price.map(function () {
+      return parseInt($(this).text());
+    }).get();
+  })
+  .then(function(prices) {
+    //log(prices);
+
+    var total = 0;
+    prices.forEach(function(p) {
+      total += p; 
+    })
+
+    //log(total / prices.length);
+    var avg = Math.round(total / prices.length);
+    log(avg);
+  });
+}
+

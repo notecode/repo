a11053695ce238bbc7a47e7e61d270fb96629998
songerwei <songerwei@xxtao.com>
2017-03-25 21:24:36 +0800

    raw

---
+++ frontend/raw/gulpfile.js
@@ -0,0 +1,47 @@
+'use strict';
+
+// https://github.com/gulpjs/gulp/blob/master/docs/API.md
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    clean = require('gulp-clean'),
+    connect = require('gulp-connect'),
+    include = require('gulp-file-include');
+
+gulp.task('connect', ['all'], function() {
+	connect.server({
+		root: 'dist',
+		livereload: true
+	});
+});
+
+gulp.task('vendor', function () {
+	return gulp.src('./vendor/**/*')
+        .pipe(gulp.dest('dist/vendor/'))
+        .pipe(connect.reload());
+});
+
+gulp.task('pages', function () {
+	return gulp.src('./src/**/*.+(html|js)')
+        .pipe(gulp.dest('dist/'))
+        .pipe(connect.reload());
+});
+gulp.task('sass', function() {
+	return gulp.src('./src/sass/style.scss')
+        .pipe(sass({includePaths: ['./src/']}).on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+	gulp.watch('./vendor/**/*', ['vendor']);
+	gulp.watch('./src/**/*.+(html|js)', ['pages']);
+	gulp.watch('./src/**/*.scss', ['sass']);
+});
+
+gulp.task('clean', function () {
+	gulp.src('dist', {read: false})
+	.pipe(clean());
+});
+
+gulp.task('all', ['vendor', 'pages', 'sass']);
+gulp.task('default', ['connect', 'watch']);

9b0b9244486c7bddef015fd4319c40a5da42b3cd
songerwei <songerwei@xxtao.com>
2017-04-11 13:28:54 +0800

    token: not refresh in every request

--- sns/backend/weixue/routes/api.php
+++ sns/backend/weixue/routes/api.php
@@ -25,7 +25,14 @@ Route::get('/users', 'Publik\UsersController@all');
 // JWTAuthController
 
 Route::post('/login', 'JWTAuth\JWTAuthController@login');
-Route::middleware('jwt.refresh')->get('/profile', 'JWTAuth\JWTAuthController@myProfile');
+
+/*
+ * @@todo:
+ * 因每次都refresh token，需要对app端的请求进行排队，否则后续请求会因token失效而失败。
+ * 故这里先不刷新token，一直用初始生成的。再完善：若实现了自动登录（将token记在本地，在每次
+ * 用户打开应用时refresh一次，以避免出现某天某时刻token突然失效的不好体验
+ */
+Route::middleware('jwt.auth')->get('/profile', 'JWTAuth\JWTAuthController@myProfile');
 
 
 // JWTRegisterController

0495fbd90037a56bc9b138b907b34070229c7767
songerwei <songerwei@xxtao.com>
2017-04-20 03:39:24 +0800

    fock images

--- sns/backend/weixue/database/seeds/PostsTableSeeder.php
+++ sns/backend/weixue/database/seeds/PostsTableSeeder.php
@@ -14,17 +14,50 @@ class PostsTableSeeder extends Seeder
         DB::table('posts')->insert([
             'author' => 1,
             'type' => 1,
-            'org_width' => 800,
-            'org_height' => 600,
+            'org_width' => 1920,
+            'org_height' => 1200,
             'data_url' => 'http://oolhommuu.bkt.clouddn.com/pikugirl.jpg',
         ]);
 
         DB::table('posts')->insert([
             'author' => 1,
             'type' => 1,
-            'org_width' => 800,
-            'org_height' => 600,
+            'org_width' => 506,
+            'org_height' => 304,
             'data_url' => 'http://oolhommuu.bkt.clouddn.com/fengjing1.JPG',
+        ]);
+
+        DB::table('posts')->insert([
+            'author' => 1,
+            'type' => 1,
+            'org_width' => 787,
+            'org_height' => 1000,
+            'data_url' => 'http://notecode.com:8080/images/me.jpg',
+        ]);
+
+        DB::table('posts')->insert([
+            'author' => 1,
+            'type' => 1,
+            'org_width' => 640,
+            'org_height' => 640,
+            'data_url' => 'http://notecode.com:8080/images/ruziniu.jpg',
+        ]);
+
+        DB::table('posts')->insert([
+            'author' => 1,
+            'type' => 1,
+            'org_width' => 3264,
+            'org_height' => 2448,
+            'data_url' => 'http://notecode.com:8080/images/ball.jpg',
+        ]);
+
+        DB::table('posts')->insert([
+            'author' => 1,
+            'type' => 1,
+            'org_width' => 440,
+            'org_height' => 594,
+            'data_url' => 'http://notecode.com:8080/images/che.jpg',
         ]);
     }
 }
+

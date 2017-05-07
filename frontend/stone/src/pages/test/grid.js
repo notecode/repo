5507aa0efa6e0e7a1f920b7b7becf88f38c1c5df
songerwei <songerwei@xxtao.com>
2017-05-07 21:59:03 +0800

    xx

--- frontend/stone/src/pages/test/grid.js
+++ frontend/stone/src/pages/test/grid.js
@@ -4,5 +4,26 @@
 
 $(function() {
 
+    var left = $('#left-bar > span');
+
+    $('#main-table').on('scroll', function() {
+        var top = $(this).scrollTop();
+        tlog(top);
+
+        left.css('top', -top+'px');
+    });
+
+    $('#triggee').toggle();
+
+    $('#trigger').click(function() {
+        $('#triggee').toggle();
+    });
+
+    $('#triggee').show(function() {
+        tlog('triggee is shown');
+    });
+    $('#triggee').hide(function() {
+        tlog('triggee is hidden');
+    });
 });
 
